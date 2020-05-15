e096d5de7c943253191ccaea5cca6805358b343f
kajman <kajmaj87@gmail.com>
2020-05-15 07:05:02 +0200

    Added inputrc for better history completion

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ ln -s ~/dotfiles/cocvimrc ~/.vim/cocvimrc
 ## bash
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/bashrc.manjaro ~/.bashrc.manjaro
+# better history completion when pressing up arrow
+ln -s ~/dotfiles/inputrc ~/.inputrc
